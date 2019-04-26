35596fd7cc961b4dd74806c2459fd5fe94f71114
Mike Doubintchik <info@allurewebsolutions.com>
2019-04-26 16:52:04 -0400

    Pulled in user data and made it available to Widget component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Widget from './components/widget';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            suggestions: [],
+            isLoading: false
+        }
+    }
+
+    componentDidMount() {
+        this.setState({isLoading: true});
+
+        axios.get('UserData.json')
+            .then(response => {
+                let massagedSuggestions = [];
+
+                response.data.forEach(person => {
+                    massagedSuggestions.push(`${person.name} (${person.username})`);
+                });
+
+                this.setState({
+                    suggestions: massagedSuggestions,
+                    isLoading: false
+                });
+            })
+            .catch(error => console.log(error));
+    }
+
+    render() {
+        const {suggestions, isLoading} = this.state;
+        
+        return (
+            <div className="App">
+                <h1>Comments</h1>
+                <Widget
+                    suggestions={suggestions}
+                    loading={isLoading}
+                />
+                <button>Add Comment</button>
+            </div>
+        )
+    }
+
 }
 
 export default App;
